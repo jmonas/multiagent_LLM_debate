d159b7a96ed515658d4f046aa48bc5ccb9265665
Jack Monas <jmonas@princeton.edu>
2024-02-07 23:54:59 -0500

    add output

--- load_models.py
+++ load_models.py
@@ -1,4 +1,15 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B", cache_dir=".cache")
-AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", cache_dir=".cache")
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B", cache_dir=".cache")
+model_inputs = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", cache_dir=".cache")
+
+tokenizer.pad_token = tokenizer.eos_token  # Most LLMs don't have a pad token by default
+model_inputs = tokenizer(
+    ["A list of colors: red, blue", "Portugal is"], return_tensors="pt", padding=True
+).to("cuda")
+generated_ids = model.generate(**model_inputs)
+generated_texts =tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+
+with open('output.txt', 'w') as file:
+    for text in generated_texts:
+        file.write(text + '\n')
