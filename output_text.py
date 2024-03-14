50da579ff3bf5f3714d92416129fb36c2fb1eb54
Jack Monas <jmonas@princeton.edu>
2024-03-14 00:57:25 -0500

    model load

--- output_text.py
+++ output_text.py
@@ -1,5 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+import time
+
 print("start")
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B", cache_dir="/scratch/network/jmonas/.cache/")
 model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B",torch_dtype=torch.float16, cache_dir="/scratch/network/jmonas/.cache/")
@@ -8,33 +10,25 @@ print("device selection")
 # Move the model to the selected device
 model.to(device)
 
-prompt = (
-    "In a shocking finding, scientists discovered a herd of unicorns living in a remote, "
-    "previously unexplored valley, in the Andes Mountains. Even more surprising to the "
-    "researchers was the fact that the unicorns spoke perfect English."
+print("Model Loaded..!")
+ 
+start_time = time.time()
+ 
+input_text = "Google was founded by"
+ 
+inputs = tokenizer(input_text, return_tensors="pt")
+input_ids = inputs["input_ids"].to("cuda")
+      
+output = model.generate(
+   input_ids,
+   attention_mask=inputs["attention_mask"].to("cuda"),
+   do_sample=True,
+   max_length=150,
+   temperature=0.8,
+   use_cache=True,
+   top_p=0.9
 )
-
-input_ids = tokenizer(prompt, return_tensors="pt").input_ids
-print("tokenizer")
-
-# Move the input IDs to the same device as the model
-input_ids = input_ids.to(device)
-
-gen_tokens = model.generate(
-    input_ids,
-    do_sample=True,
-    temperature=0.9,
-    max_length=100,
-)
-print("model generate")
-
-gen_text = tokenizer.batch_decode(gen_tokens)[0]
-print("decode")
-
-# Move the generated tokens back to CPU for decoding if you're using CUDA
-if device == torch.device("cuda"):
-    gen_text = tokenizer.batch_decode(gen_tokens.cpu())[0]
-else:
-    gen_text = tokenizer.batch_decode(gen_tokens)[0]
-
-print(gen_text)
+ 
+end_time = time.time() - start_time
+print("Total Time => ",end_time)
+print(tokenizer.decode(output[0]))
