44ddf16fb41898aa905f4107a6b86a695633f317
Jack Monas <jmonas@princeton.edu>
2024-03-14 01:16:27 -0500

    model load

--- output_text.py
+++ output_text.py
@@ -1,10 +1,10 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, GPTJForCausalLM
 import torch
 import time
 
 print("start")
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B", cache_dir="/scratch/network/jmonas/.cache/")
-model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B",cache_dir="/scratch/network/jmonas/.cache/")
+model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B",cache_dir="/scratch/network/jmonas/.cache/", revision="float16", low_cpu_mem_usage=True)
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("done")
 # print("device selection")
