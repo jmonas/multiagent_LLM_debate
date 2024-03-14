20c0063c0b7df261b6b5ff29a1f95f3ec0c31d64
Jack Monas <jmonas@princeton.edu>
2024-03-14 03:07:36 -0500

    model load

--- output_text.py
+++ output_text.py
@@ -6,7 +6,7 @@ print("start")
 tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it", cache_dir="/scratch/network/jmonas/.cache/")
 model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it", device_map="auto", torch_dtype=torch.float16,cache_dir="/scratch/network/jmonas/.cache/")
 
-input_text = "Write me a poem about Machine Learning."
+input_text = "Calculate and provide the exact numerical result for the following expression: 86+81+9."
 input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
 
 outputs = model.generate(**input_ids, max_new_tokens=150)
