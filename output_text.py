85ec858093d1cda61590a481b21bc08cd4731884
Jack Monas <jmonas@princeton.edu>
2024-03-14 01:30:34 -0500

    model load

--- output_text.py
+++ output_text.py
@@ -5,31 +5,30 @@ import time
 print("start")
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B", cache_dir="/scratch/network/jmonas/.cache/")
 model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B",cache_dir="/scratch/network/jmonas/.cache/", revision="float16", low_cpu_mem_usage=True)
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("done")
-# print("device selection")
-# # Move the model to the selected device
-# model.to(device)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+# Move the model to the selected device
+model.to(device)
 
-# print("Model Loaded..!")
+print("Model Loaded..!")
  
-# start_time = time.time()
+start_time = time.time()
  
-# input_text = "Google was founded by"
+input_text = "Google was founded by"
  
-# inputs = tokenizer(input_text, return_tensors="pt")
-# input_ids = inputs["input_ids"].to("cuda")
+inputs = tokenizer(input_text, return_tensors="pt")
+input_ids = inputs["input_ids"].to("cuda")
       
-# output = model.generate(
-#    input_ids,
-#    attention_mask=inputs["attention_mask"].to("cuda"),
-#    do_sample=True,
-#    max_length=150,
-#    temperature=0.8,
-#    use_cache=True,
-#    top_p=0.9
-# )
+output = model.generate(
+   input_ids,
+   attention_mask=inputs["attention_mask"].to("cuda"),
+   do_sample=True,
+   max_length=150,
+   temperature=0.8,
+   use_cache=True,
+   top_p=0.9
+)
  
-# end_time = time.time() - start_time
-# print("Total Time => ",end_time)
-# print(tokenizer.decode(output[0]))
+end_time = time.time() - start_time
+print("Total Time => ",end_time)
+print(tokenizer.decode(output[0]))
