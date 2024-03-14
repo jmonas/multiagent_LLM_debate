7efab12db2561058aa1bd69b00ed8614008bc9ff
Jack Monas <jmonas@princeton.edu>
2024-03-14 01:18:21 -0500

    model load

--- load_models.py
+++ load_models.py
@@ -1,5 +1,6 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, GPTJForCausalLM
 import torch
 
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B", cache_dir="/scratch/network/jmonas/.cache/")
-model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", torch_dtype=torch.float16, cache_dir="/scratch/network/jmonas/.cache/")
+# model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", torch_dtype=torch.float16, cache_dir="/scratch/network/jmonas/.cache/")
+model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B",cache_dir="/scratch/network/jmonas/.cache/", revision="float16", low_cpu_mem_usage=True)
