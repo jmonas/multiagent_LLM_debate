9b9d6df422cac39d72dd87eed126a07235f07b7f
Jack Monas <jmonas@princeton.edu>
2024-03-15 00:31:34 -0500

    try

--- generalized.py
+++ generalized.py
@@ -13,7 +13,7 @@ def format_chat(chat_history):
     return inputs
 
 def generate_round_query(answer):
-    return f"These are the recent/updated opinions from other agents: {answer} Use these opinions carefully as additional advice, can you provide an updated answer?"
+    return f"These are the recent opinions from other agents: {answer}. Use these opinions carefully as additional advice, can you provide an answer?"
 
 def clean_text(response):
     last_model_pos = response.rfind("model")
