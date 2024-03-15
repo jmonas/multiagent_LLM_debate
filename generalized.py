47b90b6eb4af27afc8933a649196ac82fe0536a7
Jack Monas <jmonas@princeton.edu>
2024-03-15 03:24:14 -0500

    try

--- generalized.py
+++ generalized.py
@@ -64,7 +64,7 @@ def run_debate(number_of_rounds, chat_history_A, chat_history_B):
         
         # Generate a response from model_A
         inputs = format_chat(chat_history_A)
-        outputs_A = model_A.generate(input_ids = inputs, max_new_tokens=300, do_sample = True, temperature = .6)
+        outputs_A = model_A.generate(input_ids = inputs, max_new_tokens=300, do_sample = True, temperature = .8)
         response_A = tokenizer.decode(outputs_A[0], skip_special_tokens=True)
         response_A_cleaned = clean_text(response_A)
 
@@ -74,7 +74,7 @@ def run_debate(number_of_rounds, chat_history_A, chat_history_B):
         
         # Generate a response from model_B
         inputs = format_chat(chat_history_B)
-        outputs_B = model_A.generate(input_ids = inputs, max_new_tokens=300, do_sample = True, temperature = .6)
+        outputs_B = model_A.generate(input_ids = inputs, max_new_tokens=300, do_sample = True, temperature = .8)
         response_B = tokenizer.decode(outputs_B[0], skip_special_tokens=True)
         response_B_cleaned = clean_text(response_B)
 
