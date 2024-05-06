3d9c15bb90b5dd88a82459079c4025f23291e5b1
Jack Monas <jmonas@princeton.edu>
2024-05-06 02:21:54 -0400

    a

--- load_models.py
+++ load_models.py
@@ -1,8 +1,8 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
-tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b-it", cache_dir="/scratch/gpfs/jmonas/.cache/", force_download=True)
-# model = AutoModelForCausalLM.from_pretrained("merve/gemma-7b-it-8bit", device_map="auto", cache_dir="/scratch/gpfs/jmonas/.cache/", force_download=True)
+# tokenizer = AutoTokenizer.from_pretrained("google/gemma-7b-it", cache_dir="/scratch/gpfs/jmonas/.cache/", force_download=True)
+model = AutoModelForCausalLM.from_pretrained("merve/gemma-7b-it-8bit", device_map="auto", cache_dir="/scratch/gpfs/jmonas/.cache/", force_download=True)
 
 # input_text = "Write me a poem about Machine Learning."
 # input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
