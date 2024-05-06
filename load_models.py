1c44fe04403ab62f6347d1720a18a6fa75d9feb6
Jack Monas <jmonas@princeton.edu>
2024-05-06 02:02:25 -0400

    a

--- load_models.py
+++ load_models.py
@@ -1,14 +1,13 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
-# import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM,BitsAndBytesConfig
+import torch
 
 quantization_config = BitsAndBytesConfig(load_in_8bit=True)
 
-tokenizer = AutoTokenizer.from_pretrained("gg-hf/gemma-7b-it")
-model = AutoModelForCausalLM.from_pretrained("gg-hf/gemma-7b-it", quantization_config=quantization_config)
+tokenizer = AutoTokenizer.from_pretrained("gg-hf/gemma-7b-it", cache_dir="/scratch/gpfs/jmonas/.cache/")
+model = AutoModelForCausalLM.from_pretrained("gg-hf/gemma-7b-it", device_map="auto",cache_dir="/scratch/gpfs/jmonas/.cache/", quantization_config=quantization_config)
 
 # input_text = "Write me a poem about Machine Learning."
 # input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
 
 # outputs = model.generate(**input_ids)
 # print(tokenizer.decode(outputs[0]))
- 
