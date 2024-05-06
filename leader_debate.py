93a613ce8ad56077efdce13fbf6c331456e0a083
Jack Monas <jmonas@princeton.edu>
2024-05-06 05:40:40 -0400

    a

--- leader_debate.py
+++ leader_debate.py
@@ -150,7 +150,7 @@ num_debates = 200
 number_of_agents = 3
 num_rounds = 3
 agents_correct = [0] * number_of_agents
-temperatures = [0.2, 0.2, .6]
+temperatures = [0.2, 0.2, .8]
 current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 json_file_path = f'results/debate_results_leader_{number_of_agents}_{num_rounds}_{current_time}_{str(temperatures)}.json'
 
