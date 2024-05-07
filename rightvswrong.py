796ee8860dc9116260f6f9cf8641bbb597be0257
Jack Monas <jmonas@princeton.edu>
2024-05-07 15:36:44 -0400

    d

--- rightvswrong.py
+++ rightvswrong.py
@@ -1,7 +1,7 @@
 import json
 
 # Path to the JSON file
-file_path = 'new_combined_2_agents_3_rounds_results.json'
+file_path = '/ debate_results_5_3_2024-05-06_05-10-51_0.4.json'
 
 # Function to read JSON data from a file
 def read_json(file_path):
