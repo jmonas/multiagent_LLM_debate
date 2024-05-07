30a5d7a83cfdd953bccd16e6527db523778da218
Jack Monas <jmonas@princeton.edu>
2024-05-07 10:29:06 -0400

    s

--- acc_measurement.py
+++ acc_measurement.py
@@ -1,7 +1,7 @@
 import json
 
 # Path to the JSON file
-file_path = 'combined_2_agents_3_rounds_results.json'
+file_path = 'new_combined_2_agents_3_rounds_results.json'
 
 # Function to read JSON data from a file
 def read_json(file_path):
