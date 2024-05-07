c795720224889cf8b7fa9113b802f03395b5b551
Jack Monas <jmonas@princeton.edu>
2024-05-07 10:41:05 -0400

    s

--- load_models.py
+++ load_models.py
@@ -1,6 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
+b = AutoTokenizer.from_pretrained("google/gemma-2b-it")
 tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it", cache_dir="/scratch/gpfs/jmonas/.cache/", force_download=True)
 model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it", torch_dtype=torch.float16,cache_dir="/scratch/gpfs/jmonas/.cache/", force_download=True)
 
